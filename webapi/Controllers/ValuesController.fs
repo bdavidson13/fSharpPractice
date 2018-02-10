f919b469fc087107599f2f1b35df67225f2d5e72
Brian Davidson <brian.j.davidson13@gmail.com>
2018-02-10 13:23:57 -0500

    Initial commit

---
+++ webapi/Controllers/ValuesController.fs
@@ -0,0 +1,32 @@
+﻿namespace fwebapi.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+type ValuesController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        // [|"value1"; "value2"|]
+        "test"
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:int) =
+        "wtf"
+
+    [<HttpPost>]
+    member this.Post([<FromBody>]value:string) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>]value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
