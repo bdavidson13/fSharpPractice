af3094b01baa98898112f466817a5c2df791c18e
Brian Davidson <brian.j.davidson13@gmail.com>
2018-02-20 08:47:54 -0500

    Added new project

---
+++ isPerfectNumber/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// Should take a number and tell whether it is
+// perfect (sum of its divisors = the number )
+// abundant soid > num
+// defecient soid < num
+
+open System
+
+let isFactor x y = x % y = 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
